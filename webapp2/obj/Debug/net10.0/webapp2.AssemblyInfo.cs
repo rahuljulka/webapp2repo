4b098f381533f15a9d6d17cd18676e015a48aa2e
Rahul Julka <rahuljulka@gmail.com>
2026-07-18 22:32:25 +0530

    kkkk

--- webapp2/obj/Debug/net10.0/webapp2.AssemblyInfo.cs
+++ webapp2/obj/Debug/net10.0/webapp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9718ffeb05a381de0ba0e8c031e9620c9b1d79a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b562a24785b471394a03d657cb0a813a48a864")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
