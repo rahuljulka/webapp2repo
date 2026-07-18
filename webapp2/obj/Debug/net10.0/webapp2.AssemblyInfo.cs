039b57b6729cf0fdb6cbacb34e6fabadc391ab2f
Rahul Julka <rahuljulka@gmail.com>
2026-07-18 21:26:22 +0530

    testfile

--- webapp2/obj/Debug/net10.0/webapp2.AssemblyInfo.cs
+++ webapp2/obj/Debug/net10.0/webapp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5859057bddcdb1631a5a6bafcfd40c084f05d23c")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
